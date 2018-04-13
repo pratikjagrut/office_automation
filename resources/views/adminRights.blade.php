b3add0b6423f0b4e4b6b33a1670df6f004dbbc33
Pratik Jagrut <pratik.jagrut111@gmail.com>
2018-04-13 12:57:19 +0530

    k

--- resources/views/adminRights.blade.php
+++ resources/views/adminRights.blade.php
@@ -60,35 +60,7 @@
 						</form>
 					</div>
 				</div>
-			</div>
-			<div class="col-md-6">
-				<div class="panel panel-default">
-					<div class="panel-heading text-center"><b>Current Admins</b></div>
-					<div class="panel-body table-responsive">
-						<table class="table table-striped">
-							@if (count($admins) > 0)
-								<tr>
-									<th>Sr. No</th>
-									<th>Employee Id</th>
-									<th>Name</th>
-									<th>Email</th>
-								</tr>
-								@foreach ($admins as $admin)
-									<tr>
-										<td>{{$loop->iteration}}</td>
-										<td>{{$admin->employee_id}}</td>
-										<td>{{ucwords($admin->name)}}</td>
-										<td>{{$admin->email}}</td>
-									</tr>
-								@endforeach
-							@endif
-						</table>
-					</div>
-				</div>
-			</div>		
-		</div>
-		<div class="row">
-			<div class="col-md-6">
+
 				<div class="panel panel-default">
 					<div class="panel-heading text-center">
 						<b>Change Employee Password</b>
@@ -129,6 +101,36 @@
 						</form>
 					</div>
 				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading text-center"><b>Current Admins</b></div>
+					<div class="panel-body table-responsive">
+						<table class="table table-striped">
+							@if (count($admins) > 0)
+								<tr>
+									<th>Sr. No</th>
+									<th>Employee Id</th>
+									<th>Name</th>
+									<th>Email</th>
+								</tr>
+								@foreach ($admins as $admin)
+									<tr>
+										<td>{{$loop->iteration}}</td>
+										<td>{{$admin->employee_id}}</td>
+										<td>{{ucwords($admin->name)}}</td>
+										<td>{{$admin->email}}</td>
+									</tr>
+								@endforeach
+							@endif
+						</table>
+					</div>
+				</div>
+			</div>		
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				
 			</div>
 			<div class="col-md-6">
 				
